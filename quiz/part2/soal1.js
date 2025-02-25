89213af7b218d3dc65c5510a6fbb13b7eed83678
brtsn15 <rioandhikap@gmail.com>
2025-02-25 14:17:03 +0700

    (soal1 part2)

---
+++ quiz/part2/soal1.js
@@ -0,0 +1,11 @@
+let word = 'JavaScript';
+let second = 'is';
+let third = 'awesome';
+let fourth = 'and';
+let fifth = 'I';
+let sixth = 'love';
+let seventh = 'it!';
+
+let kalimat = '${word} ${JavaScript} ${is} ${awesome} ${and} ${I} ${love} ${it!}';
+console.log(kalimat);
+
