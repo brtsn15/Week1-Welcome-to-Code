aa740b07a701e1524d601140458b61eb004cd3e9
brtsn15 <rioandhikap@gmail.com>
2025-02-26 13:37:38 +0700

    (soal 2 part 2)

---
+++ quiz/part2/soal2.js
@@ -0,0 +1,15 @@
+let word = 'wow JavaScript is so cool';
+
+
+let exampleFirstWord = word[0] + word[1] + word[2]; // "wow"
+let exampleSecondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13]; // "JavaScript"
+let exampleThirdWord = word[15] + word[16]; // "is"
+let exampleFourthWord = word[18] + word[19]; // "so"
+let exampleFifthWord = word[21] + word[22] + word[23] + word[24]; // "cool"
+
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + exampleSecondWord);
+console.log('Third Word: ' + exampleThirdWord);
+console.log('Fourth Word: ' + exampleFourthWord);
+console.log('Fifth Word: ' + exampleFifthWord);
